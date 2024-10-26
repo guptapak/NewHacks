d341297b6950ce7a48945a1acc570b311f359fb2
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-10-26 16:47:07 -0400

    Added the upload button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { FileUpload } from "./components/ui/file-upload";
 
 function App() {
   const [isImageUploaded, setIsImageUploaded] = useState(false);
-  const handleNormalUpload = () => {
+  const handleNormalUpload = (files: File[]) => {
     // do server upload here
-    setIsImageUploaded(true);
+    console.log(files);
+    setIsImageUploaded(false);
   };
 
   if (!isImageUploaded) {
     return (
-      <div className="flex items-center justify-center min-h-[80vh]">
+      <div className="flex items-center justify-center min-h-[90vh]">
         <div className="px-20 py-20 border-4 rounded-lg flex flex-col items-center ">
           <h2 className="text-4xl mb-12">Image Upload</h2>
-          <Button onClick={() => handleNormalUpload()}>Upload</Button>
+          <FileUpload onChange={handleNormalUpload} />
         </div>
       </div>
     );
